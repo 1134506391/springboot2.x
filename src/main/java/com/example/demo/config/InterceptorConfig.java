5dcccc4bccbc66e16ee1c05c90250739844947cb
3hpyj <3hpyj@3h.net>
2023-04-13 17:47:26 +0800

    refactor: 修改文件结构

--- src/main/java/com/example/demo/config/InterceptorConfig.java
+++ src/main/java/com/example/demo/config/InterceptorConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.config;
 
 import com.example.demo.controller.interceptor.UserInfoInterceptor;
 import org.springframework.context.annotation.Bean;
